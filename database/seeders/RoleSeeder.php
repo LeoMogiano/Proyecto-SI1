34576f3ab20ad3ac5729e176c24540bbb8f61d70
oscar <oscarorosduran@gmail.com>
2021-12-20 22:59:56 -0400

    añadi un rol mas

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -17,6 +17,7 @@ class RoleSeeder extends Seeder
     {
         $role1 = Role::create(['name'=> 'Administrador']);
         $role2 = Role::create(['name'=> 'Cliente']);
+        $role3 = Role::create(['name'=> 'Dueño']);
         Permission::create(['name' => 'gestionar usuario'])->syncRoles([$role1]);//syncRoles asigna a varios roles
         Permission::create(['name' => 'gestionar roles'])->syncRoles([$role1]);
         Permission::create(['name' => 'gestionar reserva'])->syncRoles([$role1]);
@@ -29,8 +30,10 @@ class RoleSeeder extends Seeder
         Permission::create(['name' => 'crear venta'])->syncRoles([$role2]);
         Permission::create(['name' => 'Modo Admin'])->syncRoles([$role1]);
         Permission::create(['name' => 'Modo Cliente'])->syncRoles([$role2]);
-        Permission::create(['name' => 'PermisoAdminClient'])->syncRoles([$role1,$role2]);
-        //admin
+        Permission::create(['name' => 'Modo Dueño'])->syncRoles([$role3]);
+        Permission::create(['name' => 'PermisoAdminClient'])->syncRoles([$role1,$role2,$role3]);
+
+                //admin
         //
         //
         
