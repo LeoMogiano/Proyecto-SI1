dd232e9c5fc299f9c7f6ebc553fdc95d0271b616
LeoMogiano <leomogiano@outlook.com>
2021-12-21 21:10:59 -0400

    prueba Xd

--- resources/views/dventas/create.blade.php
+++ resources/views/dventas/create.blade.php
@@ -7,18 +7,18 @@
 @stop
 
 @section('content')
-@if(session()->has('error'))
-        
-            <button type="button" class="close" data-dismiss="alert">&times;</button>
-            <strong>¡Error!</strong>  {{session()->get('error')}}
-     
-  
-  @endif
+    @if (session()->has('error'))
+
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <strong>¡Error!</strong> {{ session()->get('error') }}
+
+
+    @endif
 
     <div class="card">
 
         <div class="card-body">
- 
+
             <form method="post" action="{{ route('dventas.store') }}">
                 @csrf
 
@@ -26,14 +26,15 @@
                     <h5>Seleccionar Producto:</h5>
                     <select name="producto_id" class="focus border-primary  form-control">
                         @foreach ($productos as $producto)
-                            <option value="{{ $producto->id}}">{{ $producto->nombre }}</option>
+                            <option value="{{ $producto->id }}">{{ $producto->nombre }}</option>
                         @endforeach
 
                     </select>
                 </div>
-                    
-                    <input type="text" name="venta_id" value="{{$venta_id}}" class="focus border-primary  form-control" hidden >
-               
+
+                <input type="text" name="venta_id" value="{{ $venta_id }}" class="focus border-primary  form-control"
+                    hidden>
+
                 <div class="form-group col-md-3">
                     <h5>Cantidad:</h5>
                     <input type="text" name="cantidad" class="focus border-primary  form-control">
@@ -41,16 +42,15 @@
                 </div>
                 <h5>Descuento(Dejar en blanco si no aplica):</h5>
                 <div class="form-group d-flex col-md-1">
-                    
-                    <input type="text" name="descuento" class="focus border-primary  form-control"> <h2>%</h2>
+
+                    <input type="text" name="descuento" class="focus border-primary  form-control">
+                    <h2>%</h2>
 
                 </div>
 
-                
-
                 <br>
                 <button class="btn btn-primary" type="submit">Registrar</button>
-                <a class="btn btn-danger" href="{{ route('ventas.show',$venta_id) }}">Volver</a>
+                <a class="btn btn-danger" href="{{ route('ventas.show', $venta_id) }}">Volver</a>
                 {{-- return redirect()->route('ventas.show',$request->venta_id); --}}
             </form>
 
