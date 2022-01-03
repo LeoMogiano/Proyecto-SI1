294fb78ac67eb237c308d9ef117fc2e0382dda95
LeoMogiano <leomogiano@outlook.com>
2022-01-03 17:34:44 -0400

    arreglando https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (config(key:'app.env') == 'production') {
+            URL::forceScheme(scheme:'https');
+        }
     }
 }
