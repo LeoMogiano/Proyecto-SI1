8deb6d4ee9fc76f575c334a153cbd13122453114
LeoMogiano <leomogiano@outlook.com>
2022-01-03 21:09:40 -0400

    arreglando nube

--- database/seeders/facturaSeeder.php
+++ database/seeders/facturaSeeder.php
@@ -14,13 +14,6 @@ class facturaSeeder extends Seeder
      */
     public function run()
     {
-        $factura=New factura();
-        $factura->id=1;
-        $factura->Nro_aut=3000;
-        $factura->Fecha_f="2021-12-05 20:05:00";
-        $factura->nit=6287905;
-        $factura->monTotal=4320.2;
-        $factura->Id_venta=1;
-        $factura->save();
+        
     }
 }
--- database/seeders/ventaSeeder.php
+++ database/seeders/ventaSeeder.php
@@ -14,14 +14,6 @@ class ventaSeeder extends Seeder
      */
     public function run()
     {    
-        $venta=New venta();
-        $venta->id=1;
-       /*  $venta->Nro_v='V001'; */
-        $venta->Fecha_v="2021-12-05 15:05:00";
-        $venta->longitud="-17.795482958139004";
-        $venta->latitud="-63.177053288330086";
-        $venta->montoTotal=0;
-        $venta->Id_us=2;
-        $venta->save();
+        
     }
 }
